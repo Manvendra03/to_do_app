2e55d3a22bdc875a318abbe067203d2047a33897
Manvendra03 <manvendrapatidar03@gmail.com>
2024-10-05 16:08:04 +0530

    splash_commit

--- App.tsx
+++ App.tsx
@@ -7,15 +7,25 @@
 
 
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, { useEffect } from 'react';
 import DrawerNavigation from './src/navigation/DrawerNavigation';
 import MainNavigation from './src/navigation/MainNavigation';
+import SplashScreen from 'react-native-splash-screen';
+import { LogBox } from 'react-native';
 
 
 function App(): React.JSX.Element {
   
+
+
+LogBox.ignoreAllLogs(); 
   // return (<HomeScreen/>);
-  
+  useEffect(()=>{
+     setTimeout(()=>{
+      console.log("Started !!");
+      SplashScreen.hide();
+     },500);
+  },[]);
   return (
       <DrawerNavigation/>
       // <MainNavigation/>
